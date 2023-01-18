ad477642ea64114c826cae493992ff1973c7b119
chasedashiell <dashchax@gmail.com>
2023-01-18 11:39:14 -0600

    ArrayAssignDone2.0

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -4,18 +4,18 @@ public class ImmutableSort {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
         int[] b = new int[3];
-        for (int i = 0; i < a.length; i++){
-            for (int o = 0; o < a.length; o++){
-                if(a[i] < a[o]){
-                    int temp = a[i];
-                    a[i] = a[o];
-                    a[o] = temp;
-                }
-            }
-        }
         b[0] = a[0];
         b[1] = a[1];
         b[2] = a[2];
+        for (int i = 0; i < b.length; i++){
+            for (int o = 0; o < b.length; o++){
+                if(b[i] < b[o]){
+                    int temp = b[i];
+                    b[i] = b[o];
+                    b[o] = temp;
+                }
+            }
+        }
 
         System.out.println(Arrays.toString(b));
 
