b5ed43fc5aca1f36e4ed1b811d84c0bf5ae68478
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-21 20:50:32 -0600

    ThreeArrayRange.java feedback

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,6 +1,8 @@
+// good
 public class ThreeArrayRange {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
+        // NOTE: the correct starting values are min=a[0] and max=a[0]. Use that in future.
         int min = 10000;
         int max = 0;
         for (int i = 0; i < a.length; i++){
