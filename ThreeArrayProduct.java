a32e559164707da50393d152c61d641c916cfb3c
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-21 20:43:58 -0600

    ThreeArrayProduct.java feedback

--- ThreeArrayProduct.java
+++ ThreeArrayProduct.java
@@ -1,6 +1,7 @@
 public class ThreeArrayProduct {
     public static void main(String[] args) {
         int[] a = {2, 1, 3};
+        // FIXME: this isn't a product
         int sum = 0;
         for (int i = 0; i < a.length; i++){
             sum += a[i];
